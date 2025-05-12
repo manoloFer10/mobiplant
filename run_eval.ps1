d26607bc84d62762992a6b891e39b8e8e0db67f1
manoloFer10 <manufernandezbur@gmail.com>
2025-05-12 16:50:50 -0300

    erased .ps1 's

--- run_eval.ps1
+++
@@ -1,7 +0,0 @@
-$env:PYTHONPATH = Get-Location
-
-python eval.py `
-    --num_samples "all" `
-    --results_dataset "inference.json" `
-    --evaluation_style "automatic-metrics" `
-    --output_folder "test_eval"
