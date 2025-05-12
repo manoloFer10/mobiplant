8f6c8fd072372a5ad94d471b71bdcdaf6bb5f5e0
manoloFer10 <manufernandezbur@gmail.com>
2025-05-12 16:49:04 -0300

    merging synth results

--- helper_scripts/merge_results.py
+++ helper_scripts/merge_results.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
 results=[
-    'results_shuffled1\inference\mcq-answering_CoT_chatgpt_results.csv',
-    'results_shuffled1\inference\mcq-answering_CoT_claude_results.csv',
-    'results_shuffled1\inference\mcq-answering_CoT_gemini_results.csv',
-    'results_shuffled1\inference\mcq-answering_CoT_v3_results.csv',
-    'results_shuffled1\inference\mcq-answering_CoT_llama_results.csv',
-    'results_shuffled1\inference\mcq-answering_CoT_o1-mini_results.csv',
-    'results_shuffled1\inference\mcq-answering_CoT_r1_results.csv'
+    'synth_results\inference\mcq-answering_CoT_chatgpt_results.json',
+    'synth_results\inference\mcq-answering_CoT_claude_results.json',
+    'synth_results\inference\mcq-answering_CoT_gemini_results.json',
+    'synth_results\inference\mcq-answering_CoT_v3_results.json',
+    'synth_results\inference\mcq-answering_CoT_llama_results.json',
+    'synth_results\inference\mcq-answering_CoT_o1-mini_results.json',
+    'synth_results\inference\mcq-answering_CoT_r1_results.json'
 ]
 
-df = pd.read_csv(results[0])
+df = pd.read_json(results[0])
 
 if len(results[0:]) > 0:
     for result in results[0:]:
-        result_df = pd.read_csv(result)
+        result_df = pd.read_json(result)
         for col in result_df.columns:
             if col.startswith('CoT_reasoning_by_'): model_reasoning_column = col
             if col.startswith('CoT_election_by_'): model_election_column = col
@@ -22,4 +22,4 @@ if len(results[0:]) > 0:
         df[model_reasoning_column] = result_df[model_reasoning_column]
         df[model_election_column] = result_df[model_election_column]
 
-df.to_csv('results_shuffled1/Full_results_cot.csv', index=True)
+df.to_json('synth_results/full_results_cot.json', indent=2, orient='records')
